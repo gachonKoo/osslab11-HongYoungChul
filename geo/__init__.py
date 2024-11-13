4aedfe5991f93df556e627d5a05262fb7dcaed26
HongYoungChul <129755423+HongYoungChul@users.noreply.github.com>
2024-11-13 17:54:35 +0900

    Update __init__.py

--- geo/__init__.py
+++ geo/__init__.py
@@ -1,1 +1,9 @@
+# geo/__init__.py
+
+
+from .utils import some_function  # 예: geo/utils.py에 있는 some_function을 가져옴
+from .calculations import calculate_area  # 예: geo/calculations.py에 있는 calculate_area를 가져옴
+
+
+print("geo 패키지가 초기화되었습니다.")
 
