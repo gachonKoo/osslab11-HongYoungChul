fa0e955a9df9da74abeed9b90bbc61c76be8bef8
HongYoungChul <129755423+HongYoungChul@users.noreply.github.com>
2024-11-13 17:53:54 +0900

    Update testers.py

--- testers.py
+++ testers.py
@@ -1,1 +1,13 @@
+import math
+
+def calculate_circle(radius):
+    circumference = 2 * math.pi * radius
+    area = math.pi * (radius ** 2)
+    return circumference, area
+
+if __name__ == "__main__":
+    radius = 5
+    c, area = calculate_circle(radius)
+    print(f"c = {c}")
+    print(f"area = {area}")
 
